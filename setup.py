d306ddeb9c2f237f53a5a1e0d3309fbfd8d4688a
johnc.thomas <jct61@cam.ac.uk>
2018-10-29 12:17:31 +0000

    Revert "Made functional as an independent package, hopefully"
    This reverts commit 28c8610

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup
 
-setup(name='adjustTextOpen',
-      version='0.7.4b',
+setup(name='adjustText',
+      version='0.7.3',
       description='Iteratively adjust text position in matplotlib plots to minimize overlaps',
-      author='Ilya Flyamer, John C. Thomas',
-      author_email='flyamer@gmail.com, jcthomas000@gmail.com',
-      url='https://github.com/johncthomas/adjustTextOpen',
-      packages=['adjustTextOpen'],
-      install_requires=['numpy']
+      author='Ilya Flyamer',
+      author_email='flyamer@gmail.com',
+      url='https://github.com/Phlya/adjustText',
+      packages=['adjustText'],
+      install_requires=['numpy', 'matplotlib']
      )
